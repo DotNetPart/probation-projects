228d22914d33209ecd4b3476627769b36031c4d4
Gleb Suhanov <suhanovgleb@inbox.ru>
2018-03-18 01:25:43 +0300

    ver 1.0

---
+++ ReflectionTest/SomeLibrary/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeLibrary
+{
+    public class User
+    {
+        internal User() { }
+
+        public User(string login, string password, bool isActive = false)
+        {
+            Login = login;
+            Password = password;
+            IsActive = isActive;
+        }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; } = false;
+
+    }
+}
---
+++ ReflectionTest/SomeLibrary/UserGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeLibrary
+{
+    internal static class UserGenerator
+    {
+        internal static User CreateRandomUser()
+        {
+            User user = new User();
+            Random rnd = new Random();
+
+            user.Login = rnd.Next().ToString();
+            user.Password = rnd.Next().ToString();
+            user.IsActive = true;
+            
+            return user;
+        }
+    }
+}
