fade15d5e04acdc7ffbcd411e34ca1be54a8689b
Gleb Suhanov <suhanovgleb@inbox.ru>
2018-03-20 17:12:03 +0300

    ver 1.3

--- ReflectionTest/ReflectionConsoleTest/Program.cs
+++ ReflectionTest/ReflectionConsoleTest/Program.cs
@@ -30,20 +30,21 @@ namespace ReflectionConsoleTest
             Console.ReadKey();
         }
 
-        public static void GetReflectionInfo(string assemblyNamespace)
+        private static void GetReflectionInfo(string assemblyNamespace)
         {
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            IEnumerable<Type> types = null;
+            //IEnumerable<Type> types = null;
             foreach (var assembly in assemblies)
             {
-                types = assembly.GetTypes().Where(t => t.Namespace == assemblyNamespace);
+                var types = assembly.GetTypes().Where(t => t.Namespace == assemblyNamespace);
 
-                if (types.Any())
-                {
-                    break;
-                }
+                ConsoleReflection(types);
+                OtherConsoleReflection(types);
             }
+        }
 
+        private static void ConsoleReflection(IEnumerable<Type> types)
+        {
             foreach (Type type in types)
             {
                 IEnumerable<MemberInfo> membersInfo = type.GetTypeInfo().DeclaredMembers;
@@ -54,6 +55,24 @@ namespace ReflectionConsoleTest
                     foreach (var member in membersInfo)
                     {
                         Console.WriteLine($"----{ member.MemberType } name: { member.Name }");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private static void OtherConsoleReflection(IEnumerable<Type> types)
+        {
+            foreach (Type type in types)
+            {
+                IEnumerable<MemberInfo> membersInfo = type.GetTypeInfo().DeclaredMembers;
+                if (type.IsClass)
+                {
+                    Console.WriteLine($"##Class: { type.Name }");
+
+                    foreach (var member in membersInfo)
+                    {
+                        Console.WriteLine($"####{ member.MemberType }: { member.Name }");
                     }
                     Console.WriteLine();
                 }
